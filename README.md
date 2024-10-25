505da77c85d9e4c0c79f38b814dc607df8519b09
Paul Reiners <reine097@umn.edu>
2024-10-25 16:04:40 -0500

    Forgot to let the user specify the number of edges.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # graph-theory
 Graph theory exercises
+
+## Random graphs
+### [ErdosRenyiGraph](./src/main/java/com/leipzig48/ErdosRenyiGraph.java)
+
+Takes integer values <i>V</i> and <i>E</i> from the command line and builds a graph by generating <i>E</i> random pairs of integers between <i>0</i> and <i>V - 1</i>.  <i>Note:</i> This generator produces self-loops and parallel edges.
--- src/main/java/com/leipzig48/ErdosRenyiGraph.java
+++ src/main/java/com/leipzig48/ErdosRenyiGraph.java
@@ -1,11 +1,16 @@
-package edu.com.leipzig48;
+package com.leipzig48;
 
 import edu.princeton.cs.algs4.Graph;
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 import java.util.Random;
 
 
+/**
+ * Takes integer values <i>V</i> and <i>E</i> from the command line and builds a
+ * graph by generating <i>E</i> random pairs of integers between <i>0</i> and 
+ * <i>V - 1</i>.  <i>Note:</i> This generator produces self-loops and parallel 
+ * edges.
+ */
 public class ErdosRenyiGraph {
     /**
      * Unit tests the {@code ErdosRenyiGraph} data type.
@@ -14,6 +19,7 @@ public class ErdosRenyiGraph {
      */
     public static void main(String[] args) {
         int V = Integer.parseInt(args[0]);
+        int E = Integer.parseInt(args[1]);
         Graph graph = new Graph(V);
         for (int i = 0; i < V; i++) {
             Random rand = new Random();
