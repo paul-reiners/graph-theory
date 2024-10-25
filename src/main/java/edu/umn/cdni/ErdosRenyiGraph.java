a445a4eef7b3b38d55a1a637d4c1233821aa4165
Paul Reiners <reine097@umn.edu>
2024-10-25 14:59:59 -0500

    First commit.

---
+++ src/main/java/edu/umn/cdni/ErdosRenyiGraph.java
@@ -0,0 +1,28 @@
+package edu.umn.cdni;
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Random;
+
+
+public class ErdosRenyiGraph {
+    /**
+     * Unit tests the {@code ErdosRenyiGraph} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        int V = Integer.parseInt(args[0]);
+        Graph graph = new Graph(V);
+        for (int i = 0; i < V; i++) {
+            Random rand = new Random();
+  
+            int rand_int1 = rand.nextInt(V);
+            int rand_int2 = rand.nextInt(V);
+
+            graph.addEdge(rand_int1, rand_int2);
+        }
+        StdOut.println(graph);
+    }
+}
