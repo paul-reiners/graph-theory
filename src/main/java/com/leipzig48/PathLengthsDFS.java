0e9d65df8ba2de7dd52313ad65e21d6726afd53c
Paul Reiners <reine097@umn.edu>
2024-10-28 15:14:44 -0500

    Wrote stats to file.

--- src/main/java/com/leipzig48/PathLengthsDFS.java
+++ src/main/java/com/leipzig48/PathLengthsDFS.java
@@ -2,37 +2,49 @@ package com.leipzig48;
 
 import java.util.Random;
 import java.util.stream.StreamSupport;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import edu.princeton.cs.algs4.DepthFirstPaths;
 import edu.princeton.cs.algs4.Graph;
 
 public class PathLengthsDFS {
     public static void main(String[] args) {
-        Random rand = new Random();
-        for (int v = 1; v < 256; v++) {
-            System.out.println("v: " + v);
-            for (int e = 0; e < 256; e++) {
-                System.out.println("\te: " + e);
-                int count = 0;
-                long totalLength = (long) 0;
-                for (int i = 0; i < 128; i++) {
-                    ErdosRenyiGraph erdosRenyiGraph = new ErdosRenyiGraph(v, e);
-                    Graph graph = erdosRenyiGraph.getGraph();
-                    int s = rand.nextInt(v);
-                    int vertex2 = rand.nextInt(v);
-        
-                    DepthFirstPaths depthFirstPaths = new DepthFirstPaths(graph, s);
-                    boolean found =  depthFirstPaths.hasPathTo(vertex2);
-                    if (found) {
-                        count++;
-                        Iterable<Integer> path =  depthFirstPaths.pathTo(vertex2);
-                        long length = StreamSupport.stream(path.spliterator(), false).count();
-                        totalLength += length;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data/out/erdos_renyi_graph/path_lengths_dfs/stats.csv", true))) {
+            writer.write("V,E,fraction_connected,average_path_length");
+            writer.newLine();
+          
+            Random rand = new Random();
+            for (int v = 1; v < 256; v++) {
+                System.out.println("v: " + v);
+                for (int e = 0; e < 256; e++) {
+                    System.out.println("\te: " + e);
+                    int count = 0;
+                    long totalLength = (long) 0;
+                    for (int i = 0; i < 128; i++) {
+                        ErdosRenyiGraph erdosRenyiGraph = new ErdosRenyiGraph(v, e);
+                        Graph graph = erdosRenyiGraph.getGraph();
+                        int s = rand.nextInt(v);
+                        int vertex2 = rand.nextInt(v);
+            
+                        DepthFirstPaths depthFirstPaths = new DepthFirstPaths(graph, s);
+                        boolean found =  depthFirstPaths.hasPathTo(vertex2);
+                        if (found) {
+                            count++;
+                            Iterable<Integer> path =  depthFirstPaths.pathTo(vertex2);
+                            long length = StreamSupport.stream(path.spliterator(), false).count();
+                            totalLength += length;
+                        }
                     }
-                }
 
-                System.out.println("\t\tFraction: " + ((double) count / 128.0) + "; Average length: " + ((double) totalLength / 128.0));
+                    System.out.println("\t\tFraction: " + ((double) count / 128.0) + "; Average length: " + ((double) totalLength / 128.0));
+                    writer.write(String.format("%d,%d,%f,%f", v, e, ((double) count / 128.0), ((double) totalLength / 128.0)));
+                    writer.newLine();
+                }
             }
+        } catch (IOException ex) {
+              ex.printStackTrace();
         }
     }
 }
