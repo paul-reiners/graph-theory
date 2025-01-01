b4baa203b84e0146a7a81497c7c13ef9fae98f56
Paul Reiners <paul.reiners@gmail.com>
2025-01-01 10:13:23 -0600

    Define fitness function

--- src/main/python/vnc_matching_challenge/genetic_algorithm.py
+++ src/main/python/vnc_matching_challenge/genetic_algorithm.py
@@ -2,3 +2,11 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 from prettytable import PrettyTable
+
+from src.main.python.vnc_matching_challenge.calculate_alignment_score import calculate_alignment
+
+
+# Define the fitness function (objective with the objective of aligning the
+# connectivity between the two graph as closely as possible.
+def fitness_function(male_edges, female_edges, matching):
+    return calculate_alignment(male_edges, female_edges, matching)
